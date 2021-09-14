2cfdfeed62bb5b032a029f93964badc4fbc85aa4
Miguel B. Teruel <mteruel@debx64template.twu.edu>
2021-09-14 10:43:02 -0500

    bear

---
+++ Labs/madlib.pl
@@ -0,0 +1,26 @@
+o#!/usr/bin/perl
+
+print "\n please enter adjective.\n";
+
+$adjective=<STDIN>;
+
+print "\n please enter noun.\n";
+
+$noun1=<STDIN>;
+
+print "\n please enter verb.\n";
+
+$verb1=<STDIN>;
+
+print "\n please enter different noun.\n";
+
+$noun2=<STDIN>;
+
+print "\n please enter another  verb.\n";
+
+$verb2=<STDIN>;
+
+
+print "My name is $noun1 and I $verb1 with $adjective $noun2.\n";
+
+print "$noun2 loves to $verb2.\n"; 
