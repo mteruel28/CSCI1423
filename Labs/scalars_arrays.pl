eb7da1091d6f6bb05b0f1097684e734b8ea3eb0e
Miguel B. Teruel <mteruel@debx64template.twu.edu>
2021-09-16 10:48:17 -0500

    scalars

---
+++ Labs/scalars_arrays.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perll
+
+use warnings;
+
+# Variable Declartion (and Initialization)
+$name= "Maiko";
+$animal= "dog";
+$breed= "beagle";
+
+print "name=$name\n";
+print "animal=$animal\n";
+print "breed=$breed\n";
+
+
