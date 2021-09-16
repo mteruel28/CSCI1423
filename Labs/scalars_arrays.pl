ef87db6d264f2f8304871d261052cd4404135e58
Miguel B. Teruel <mteruel@debx64template.twu.edu>
2021-09-16 11:10:01 -0500

    Array stacks/queue

--- Labs/scalars_arrays.pl
+++ Labs/scalars_arrays.pl
@@ -6,9 +6,20 @@ use warnings;
 $name= "Maiko";
 $animal= "dog";
 $breed= "beagle";
+@any =("cat",2,"white");
 
 print "name=$name\n";
 print "animal=$animal\n";
 print "breed=$breed\n";
+print "@any\n";
 
+#Using an Array as a Stack
+push(@any,$name);
+print "@any\n"; 
+pop (@any);
+print "@any\n";
 
+#Using an Array as a Queue
+shift(@any);
+push(@any,"corgi");
+print"@any\n";
