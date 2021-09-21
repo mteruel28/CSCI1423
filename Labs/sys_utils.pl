dee07e81b1cb8f8d0f4ec223dfacdef7abffbe8f
Miguel B. Teruel <mteruel@debx64template.twu.edu>
2021-09-21 09:19:55 -0500

    sys_utils.pl

---
+++ Labs/sys_utils.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+#if(user wants to update);
+
+my $a = 1;
+
+my $b = 2;
+
+#if condition to check
+# for 1
+
+if ($a= 1){ 
+
+	system( "sudo apt-get update command");
+}
+
+if ($b= 2){
+
+	system( "passwd command" );
+
+}
