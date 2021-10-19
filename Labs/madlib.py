90dbc8dea98a639b2fb58903c28125fe404819f9
Miguel B. Teruel <mteruel@debx64template.twu.edu>
2021-10-19 11:54:19 -0500

    madlib.py

---
+++ Labs/madlib.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+print("give me five words for a story");
+
+noun = input("enter a noun:")
+verb = input("enter a verb:")
+adjective = input("enter a adjective:")
+adverb = input ("enter a adverb:")
+noun2 = input("enter another noun:")
+
+print(" The " + " " + adjective + " " + noun + " " + adverb + " " + verb + " " +  noun2 + " .");
