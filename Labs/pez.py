7f6bda1bb5a335636b2bc7dc6eee6aef157409b9
Miguel Teruel <Miguel@MacBook-Pro-2.local>
2021-10-25 20:30:21 -0500

    pez.py

--- Labs/pez.py
+++ Labs/pez.py
@@ -9,6 +9,41 @@ for Pez in range (1,10):
     print(mypez)
 
 first_item = mypez.pop(0) #first pez
-second_element =mypez.pop(0) #second pez
-print (first_item)
+print(first_item)
+
+print(mypez)
+second_element = mypez.pop(0)#second pez
 print(second_element)
+
+print(mypez)
+third_element=mypez.pop(0)
+print(third_element)
+
+print(mypez)
+fourth_element=mypez.pop(0)
+print(fourth_element)
+
+print(mypez)
+fifth_element=mypez.pop(0)
+print(fifth_element)
+
+print(mypez)
+sixth_element=mypez.pop(0)
+print(sixth_element)
+
+print(mypez)
+seventh_element=mypez.pop(0)
+print(seventh_element)
+
+print(mypez)
+eighth_element=mypez.pop(0)
+print(eighth_element)
+
+print(mypez)
+nineth_element=mypez.pop(0)
+print(nineth_element)
+
+print(mypez)
+tenth_element=mypez.pop(0)
+print(tenth_element)
+
