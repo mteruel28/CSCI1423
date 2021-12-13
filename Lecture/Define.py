3b5e9e0992a1279f357d9d1e8be0224af24b37ea
Miguel Teruel <mteruel@twu.edu>
2021-12-13 18:04:13 +0000

    Update Lecture/Define.py

--- Lecture/Define.py
+++ Lecture/Define.py
@@ -1,11 +1,1 @@
-def mail(firstname,lastname):
-    print ("Your TWU email is " + firstname[0] + lastname + "@twu.edu")
 
-mail(firstname = "miguel" ,lastname = "teruel")
-
-
-def mail2():
-    first =input("Your first name please:")
-    last =input("Your last name too:")
-    print("Your TWU email is:"+ first(0)+last+"@twu.edu")
-mail2()
